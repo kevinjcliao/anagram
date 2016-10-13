3f9f60dc298641c980e0e1616a6f0367e138c613
Kevin Liao <me@liaokev.in>
2016-10-13 19:16:11 -0400

    Handles new string edit events.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleNewString(e, inputId){
+    // TODO: Edit state
+    console.log(e.target.value);
+    console.log(inputId);
+    let update = {};
+    update[inputId] = e.target.value;
+    /**
+    this.setState(update, () => {
+      console.log("State is now: ", this.state);
+    });
+    **/
+    // TODO: Feed new state info as callback to isAnagram.
+    // TODO: Set result of isAnagram in state.
+  }
   isAnagram(string1, string2) {
     const sortedString1 = this.removePunctuation(string1)
                               .split("")
@@ -19,7 +33,7 @@ class App extends Component {
     return s.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ");
   }
   render() {
-    console.log(this.isAnagram("hello", "olleh"));
+    // TODO: Handle Binding properly.
     return (
       <div className="App">
         <div className="App-header">
@@ -27,7 +41,8 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <input type="text" onChange={this.handleNewString.bind("input1")} />
+          <input type="text" onChange={this.handleNewString.bind("input2")} />
         </p>
       </div>
     );
